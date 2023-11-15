6d1ac863bb637110404e51d918ac9846c43155e6
Dongryul Lee <al7ech@gmail.com>
2023-11-15 10:53:03 +0100

    add log

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os
 import urllib
+import traceback
 from flask import Flask, request
 import telegram
 
@@ -12,20 +13,27 @@ def check_auth(username, password):
 
 @app.route(f'/{os.getenv("ENDPOINT", default="alert")}', methods=["POST"])
 async def alert(*kargs, **kwargs):
-    auth = request.authorization
-    if not auth or not check_auth(auth.username, auth.password):
-        return {'message': 'Authentication required'}, 401
+    try:
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return {'message': 'Authentication required'}, 401
 
-    title = request.json['title']
-    content = request.json['content']
+        title = request.json['title']
+        content = request.json['content']
 
-    for c in ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']:
-        content = content.replace(c, f'\\{c}')
+        for c in ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!']:
+            content = content.replace(c, f'\\{c}')
 
-    text = "\n".join([f"*{title}*", f"{content}"])
+        text = "\n".join([f"*{title}*", f"{content}"])
 
-    async with bot:
-        for user_id in os.getenv("RECEIVER_ID", default="").split():
-            await bot.send_message(text=text, chat_id=user_id, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
+        async with bot:
+            for user_id in os.getenv("RECEIVER_ID", default="").split():
+                await bot.send_message(text=text, chat_id=user_id, parse_mode=telegram.constants.ParseMode.MARKDOWN_V2)
 
-    return {"status": "OK"}, 200
+        return {"status": "OK"}, 200
+
+    except exception as e:
+        print(str(e))
+        traceback.print_exc()
+
+        return {'message': 'Internal Server Error'}, 500
